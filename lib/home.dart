f67a00195b53bdc3f1d43c7c340160dd0f19fd42
urquico <kurtjacob.urquico@gmail.com>
2023-10-03 19:01:05 +0800

    final shit

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:getwidget/getwidget.dart';
+
+import 'package:quiz_app/start_game.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,71 +11,156 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<dynamic> categories = [];
   bool showBtn = false;
 
-  ScrollController scrollController = ScrollController();
+  List<Map<String, dynamic>> categories = [
+    {
+      'category': 'General Knowledge',
+      'categoryId': 9,
+      'description':
+          'General Knowledge questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.',
+    },
+    {
+      'category': 'Books',
+      'categoryId': 10,
+      'description':
+          'Books and Authors questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.',
+    },
+    {
+      'category': 'Films',
+      'categoryId': 11,
+      'description':
+          'Film questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.',
+    },
+    {
+      'category': 'Music',
+      'categoryId': 12,
+      'description':
+          'Music questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.',
+    },
+    {
+      'category': 'Musical & Theatres',
+      'categoryId': 13,
+      'description':
+          "Musical & Theatres questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.",
+    },
+    {
+      'category': 'Television',
+      'categoryId': 14,
+      'description':
+          'Television questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.',
+    },
+    {
+      'category': 'Video Games',
+      'categoryId': 15,
+      'description':
+          'Video Games questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.',
+    },
+    {
+      'category': 'Board Games',
+      'categoryId': 16,
+      'description':
+          'Board Games questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.',
+    },
+    {
+      'category': 'Science & Nature',
+      'categoryId': 17,
+      'description':
+          'Science & Nature questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.',
+    },
+    {
+      'category': 'Computer',
+      'categoryId': 18,
+      'description':
+          'Computer questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+    {
+      'category': 'Mathematics',
+      'categoryId': 19,
+      'description':
+          'Mathematics questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+    {
+      'category': 'Mythology',
+      'categoryId': 20,
+      'description':
+          'Mythology questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+    {
+      'category': 'Sports',
+      'categoryId': 21,
+      'description':
+          'Sports questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+    {
+      'category': 'Geography',
+      'categoryId': 22,
+      'description':
+          'Geography questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+    {
+      'category': 'History',
+      'categoryId': 23,
+      'description':
+          'History questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+    {
+      'category': 'Politics',
+      'categoryId': 24,
+      'description':
+          'Politics questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+    {
+      'category': 'Celebrities',
+      'categoryId': 26,
+      'description':
+          'Celebrities questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+    {
+      'category': 'Animals',
+      'categoryId': 27,
+      'description':
+          'Animals questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+    {
+      'category': 'Vehicles',
+      'categoryId': 28,
+      'description':
+          'Vehicles questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+    {
+      'category': 'Comics',
+      'categoryId': 29,
+      'description':
+          'Comics questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+    {
+      'category': "Japan's Anime & Manga",
+      'categoryId': 31,
+      'description':
+          'Anime and Manga questions and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+    {
+      'category': "Cartoon & Animations",
+      'categoryId': 32,
+      'description':
+          'Cartoon and Animations question and answers with explanation for interview, competitive examination and entrance test. Fully solved examples with detailed answer description, explanation are given and it would be easy to understand.'
+    },
+  ];
 
   @override
   void initState() {
-    fetchCategories();
-    scrollController.addListener(() {
-      //scroll listener
-      double showoffset =
-          10.0; //Back to top botton will show on scroll offset 10.0
-
-      if (scrollController.offset > showoffset) {
-        showBtn = true;
-        setState(() {
-          //update state
-        });
-      } else {
-        showBtn = false;
-        setState(() {
-          //update state
-        });
-      }
-    });
+    // fetchCategories();
     super.initState();
-  }
-
-  Future<void> fetchCategories() async {
-    final response =
-        await http.get(Uri.parse('https://the-trivia-api.com/v2/tags'));
-
-    if (response.statusCode == 200) {
-      final List newCategories = json.decode(response.body);
-      // Successful response, parse and handle the data here
-      setState(() {
-        categories = newCategories.map<String>((category) {
-          final categoryName = category;
-          return "$categoryName";
-        }).toList();
-      });
-    } else {
-      print(response);
-    }
   }
 
   @override
   Widget build(BuildContext context) {
-    print(categories.isEmpty);
+    double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
       extendBodyBehindAppBar: true,
-      floatingActionButton: FloatingActionButton.small(
-        onPressed: () {
-          scrollController.animateTo(
-              //go to top of scroll
-              0, //scroll offset to go
-              duration: Duration(milliseconds: 500), //duration of scroll
-              curve: Curves.fastOutSlowIn //scroll type
-              );
-        },
-        tooltip: 'Back to top',
-        child: const Icon(Icons.arrow_upward),
-      ),
       appBar: AppBar(
         title: Container(
           padding: const EdgeInsets.only(
@@ -92,7 +178,6 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.transparent,
       ),
       body: SingleChildScrollView(
-        controller: scrollController,
         child: Container(
           // blue gradient accent background color
           decoration: const BoxDecoration(
@@ -147,53 +232,93 @@ class _HomeState extends State<Home> {
                   const SizedBox(
                     height: 50,
                   ),
-                  const Center(
-                    child: Text(
-                      "Choose a Category",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 20,
-                        fontFamily: 'DM Sans',
-                        color: Colors.white,
-                        decoration: TextDecoration.none,
-                        // add padding
-                      ),
+                  const Text(
+                    "Choose a Category",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                      fontFamily: 'DM Sans',
+                      color: Colors.white,
+                      decoration: TextDecoration.none,
+                      // add padding
                     ),
                   ),
 
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      if (categories.isEmpty)
-                        const Column(
-                          children: [
-                            SizedBox(
-                              height: 50,
-                            ),
-                            Center(
-                              child: CircularProgressIndicator(
-                                color: Colors.white,
+                      ListView.builder(
+                        shrinkWrap:
+                            true, // Use shrinkWrap to allow the ListView to take only the space it needs
+                        itemCount: categories.length,
+                        itemBuilder: (context, index) {
+                          return SizedBox(
+                            child: GFAccordion(
+                              titleChild: Column(
+                                children: [
+                                  Text(
+                                    categories[index]['category'],
+                                    textAlign: TextAlign.start,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      fontFamily: 'DM Sans',
+                                      color: Colors.black,
+                                      decoration: TextDecoration.none,
+                                    ),
+                                  ),
+                                  const Text(
+                                    "10 Questions",
+                                    textAlign: TextAlign.start,
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      fontFamily: 'DM Sans',
+                                      color: Colors.black,
+                                      decoration: TextDecoration.none,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              contentChild: Column(
+                                children: [
+                                  Text(
+                                    categories[index]['description'],
+                                    style: const TextStyle(
+                                      fontSize: 15,
+                                      fontFamily: 'DM Sans',
+                                      color: Colors.black,
+                                      decoration: TextDecoration.none,
+                                    ),
+                                  ),
+                                  GFButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => StartGame(
+                                            category: categories[index]
+                                                ['category'],
+                                            categoryId: categories[index]
+                                                ['categoryId'],
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    text: 'Start Quiz',
+                                    fullWidthButton: true,
+                                    textStyle: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'DM Sans',
+                                      color: Colors.white,
+                                      decoration: TextDecoration.none,
+                                    ),
+                                  )
+                                ],
                               ),
                             ),
-                          ],
-                        )
-                      else
-                        RefreshIndicator(
-                            onRefresh: fetchCategories,
-                            child: ListView.builder(
-                              shrinkWrap:
-                                  true, // Use shrinkWrap to allow the ListView to take only the space it needs
-                              itemCount: categories.length,
-                              itemBuilder: (context, index) {
-                                return Container(
-                                  height: 50,
-                                  color: Colors.amber[600],
-                                  child: Center(
-                                    child: Text(categories[index]),
-                                  ),
-                                );
-                              },
-                            ))
+                          );
+                        },
+                      ),
                     ],
                   ),
                 ],
--- lib/start_game.dart
+++ lib/start_game.dart
@@ -1,34 +1,204 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/choices.dart';
-import 'package:quiz_app/questions.dart';
+import 'package:getwidget/getwidget.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:math';
 
 class StartGame extends StatefulWidget {
   final String category;
+  final int categoryId;
 
-  const StartGame({Key? key, required this.category}) : super(key: key);
+  const StartGame({Key? key, required this.category, required this.categoryId})
+      : super(key: key);
 
   @override
-  State<StartGame> createState() => _StartGameState(category: category);
+  State<StartGame> createState() =>
+      // ignore: no_logic_in_create_state
+      _StartGameState(category: category, categoryId: categoryId);
 }
 
 class _StartGameState extends State<StartGame> {
   final String category;
+  final int categoryId;
+  bool showScore = false;
+  int score = 0;
+  Map<String, dynamic> quizData = {};
+  List<dynamic> selectedAnswers =
+      List.filled(10, -1); // Assuming there are 10 questions
+  // List<dynamic> correctAnswers =
+  //     List.generate(10, (index) => Random().nextInt(4));
 
-  _StartGameState({required this.category});
+  _StartGameState({required this.category, required this.categoryId});
 
   @override
   void initState() {
+    fetchCategories(categoryId: categoryId.toString());
     super.initState();
+  }
+
+  Future<void> fetchCategories({required String categoryId}) async {
+    final url = Uri.parse(
+        'https://opentdb.com/api.php?amount=10&category=$categoryId&difficulty=medium&type=multiple');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      setState(() {
+        quizData = jsonDecode(response.body);
+      });
+    } else {
+      print(response);
+    }
+  }
+
+  int countThrees(List<dynamic> numbers) {
+    int count = 0;
+
+    for (int number in numbers) {
+      if (number == 3) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+
+  String decodeHtmlEntities(String input) {
+    final StringBuffer output = StringBuffer();
+    int i = 0;
+
+    while (i < input.length) {
+      if (input.startsWith('&quot;', i)) {
+        output.write('"');
+        i += 6; // Move past '&quot;'
+      } else if (input.startsWith('&ldquo;', i)) {
+        output.write('“'); // Left double quotation mark
+        i += 7; // Move past '&ldquo;'
+      } else if (input.startsWith('&#039;', i)) {
+        output.write("'");
+        i += 6; // Move past '&#039;'
+      } else {
+        // Copy the character as is
+        output.write(input[i]);
+        i++;
+      }
+    }
+
+    return output.toString();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(category),
-      ),
-      // Add content inside the body
-    );
+        appBar: AppBar(
+          title: Text(category),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.only(
+              top: 10,
+              left: 30,
+              right: 30,
+            ),
+            child: Column(
+              children: [
+                GFAlert(
+                  title: 'Welcome to $category Quiz!',
+                  content: "Answer the following 10 questions",
+                  type: GFAlertType.rounded,
+                ),
+                const SizedBox(height: 20),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: quizData.containsKey('results')
+                      ? quizData['results'].length
+                      : 0,
+                  itemBuilder: (context, index) {
+                    if (quizData.containsKey('results') == true) {
+                      return Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              decodeHtmlEntities(
+                                  quizData['results'][index]['question']),
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                                fontFamily: 'DM Sans',
+                                color: Colors.blueGrey,
+                                decoration: TextDecoration.none,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: quizData['results'][index]
+                                        ['incorrect_answers']
+                                    .length +
+                                1,
+                            itemBuilder: (context, choiceIndex) {
+                              return GFCheckboxListTile(
+                                title: Text(
+                                  choiceIndex == 3
+                                      ? decodeHtmlEntities(quizData['results']
+                                          [index]['correct_answer'])
+                                      : decodeHtmlEntities(quizData['results']
+                                              [index]['incorrect_answers']
+                                          [choiceIndex]),
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: 'DM Sans',
+                                    color: Colors.blueGrey,
+                                    decoration: TextDecoration.none,
+                                  ),
+                                ),
+                                value: selectedAnswers[index] == choiceIndex,
+                                onChanged: (value) {
+                                  setState(() {
+                                    selectedAnswers[index] = choiceIndex;
+                                  });
+                                },
+                                activeBgColor: Colors.deepPurple,
+                                inactiveBorderColor: Colors.deepPurple,
+                                activeBorderColor: Colors.deepPurple,
+                              );
+                            },
+                          ),
+                          const SizedBox(height: 20),
+                        ],
+                      );
+                    } else {
+                      // Handle the case when quizData is null or does not contain 'results'
+                      return const Text('No quiz data available');
+                    }
+                  },
+                ),
+                Container(
+                    child: showScore
+                        ? GFAlert(
+                            content: 'Your score is $score / 10',
+                            type: GFAlertType.rounded,
+                          )
+                        : null),
+                const SizedBox(height: 20),
+                GFButton(
+                  onPressed: () {
+                    setState(() {
+                      showScore = true;
+                      score = countThrees(selectedAnswers);
+                    });
+                  },
+                  text: 'Submit',
+                  fullWidthButton: true,
+                ),
+              ],
+            ),
+          ),
+        )
+        // Add content inside the body
+        );
   }
 }
